8884154776871c65c43177471926d9846735d875
hubin <hubin.android@gmail.com>
2019-09-24 21:55:33 +0800

    Spring Security Demo 01;

--- spring-security-01/src/main/java/com/ihubin/springsecurity/SpringSecurity01Application.java
+++ spring-security-01/src/main/java/com/ihubin/springsecurity/SpringSecurity01Application.java
@@ -3,6 +3,9 @@ package com.ihubin.springsecurity;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+//@SpringBootApplication(exclude = {
+//        org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration.class
+//})
 @SpringBootApplication
 public class SpringSecurity01Application {
 
---
+++ spring-security-01/src/main/java/com/ihubin/springsecurity/controller/TestController.java
@@ -0,0 +1,14 @@
+package com.ihubin.springsecurity.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @GetMapping("test")
+    public String test() {
+        return "Spring Security!";
+    }
+
+}
